35241ce141178ebb0438b1d506c420c06fbb7f85
Pacleb, Alyssa June N <ajn873@lib-pclem026.local>
2019-03-05 13:26:09 -0600

    Images and code but not actually working

---
+++ Assignment5b/AnimalsTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  AnimalsTableViewController.swift
+//  Assignment5b
+//
+//  Created by Pacleb, Alyssa June N on 3/5/19.
+//  Copyright © 2019 Pacleb, Alyssa June N. All rights reserved.
+//
+
+import UIKit
+struct whiteCells {
+    var id: Int
+    var text: String
+    var image: String
+}
+class AnimalsTableViewController: UITableViewController {
+
+    var whitecells = [
+        whiteCells(id:1, text:"Some text 1", image: "Cakes"),
+        whiteCells(id:2, text:"Some text 2", image: "Cookies"),
+        whiteCells(id:3, text:"Some text 3", image: "CupOfCoffee"),
+        whiteCells(id:4, text:"Some text 4", image: "CupOfTea")
+    ]
+    /*
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }*/
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return whitecells.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+
+        let whitecell = whitecells[indexPath.row]
+        cell.detailTextLabel?.text = whitecell.text
+        cell.imageView?.image = UIImage(named: whitecell.image)
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
