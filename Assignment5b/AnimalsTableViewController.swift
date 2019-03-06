030c3f49a4893536c97f5b2250f225adfd547756
Pacleb, Alyssa June N <ajn873@lib-pclem027.lib.utexas.edu>
2019-03-06 12:38:39 -0600

    cell length

--- Assignment5b/AnimalsTableViewController.swift
+++ Assignment5b/AnimalsTableViewController.swift
@@ -52,7 +52,7 @@ class AnimalsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 8
+        return 2*animals.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
