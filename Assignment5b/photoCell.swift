160faf1d026c6339a7d071102f6d2a04cc5fe3c8
Pacleb, Alyssa June N <ajn873@lib-pclem053.lib.utexas.edu>
2019-03-07 13:02:11 -0600

    Collection view

---
+++ Assignment5b/photoCell.swift
@@ -0,0 +1,14 @@
+//
+//  photoCell.swift
+//  Assignment5b
+//
+//  Created by Pacleb, Alyssa June N on 3/7/19.
+//  Copyright © 2019 Pacleb, Alyssa June N. All rights reserved.
+//
+
+import UIKit
+
+class photoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
